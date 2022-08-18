e5c54b0e1cb8c82f90140a7f86e8b927032c2a2d
Miles Granger <miles59923@gmail.com>
2022-08-18 12:08:24 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup, find_namespace_packages, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='package',
     packages=find_namespace_packages(
         include=['package*'], exclude=["package.tests*"]
     ),
-    zip_safe=False,
-    package_data={"package": ["*.pyx"]},
     include_package_data=True,
-    test_suite="package.tests"
 )
 
